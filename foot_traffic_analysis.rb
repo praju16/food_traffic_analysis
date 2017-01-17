f6c138e3bd634cf5802947e95b53ce9bd5a3a140
prajwal <prajwal.s.16@gmail.com>
2017-01-17 08:34:35 +0530

    process file

--- foot_traffic_analysis.rb
+++ foot_traffic_analysis.rb
@@ -1,36 +1,56 @@
-# require './input_1.txt'
-require 'set'
+# require './file_proccessor.rb'
+# require 'set'
 
 class FootTrafficAnalysis
   
   def initialize(file)
-    @file = File.open(file).shift
+    @file = file
     @rooms = []
     @visitors = []
-    sdafdf
+    @lines = []
   end 
 
   def generate_report
-    @file.each_line do |line|
-       process_log(line)
-    end
+    file_obj = FileProccesor.new(@file)
+    @lines = file_obj.process_input
   end
 
-
   def process_log(data)
-    visitor_no, room_no, visitor_activity, timestamp = data.split 
-    logic(visitor_no,room_no,visitor_activity,timestamp)
+    
   end
 
 
   def logic(visitor_no,id,activity,timestamp)
-    if @rooms[id].nil?
-      @rooms[id]= Set.new()
-      @rooms[id][:vistior_count] +=1 if activity == 'I'
-    end 
-    @rooms[id][:total_time] = 0 if rooms[id][:total_time].nil?
   end
 
+end
+
+class FileProccesor
+    
+  def initialize(file=nil)
+    @file = file
+  end 
+
+  def process_input
+    lines = []
+    file = File.open(@file)
+    lines = file_to_array_convert(file,lines)
+    break_arr_component(lines)
+  end
+
+  def file_to_array_convert(file,lines)
+    file.each_line do |line|
+      lines << line.chomp
+    end
+    lines
+  end
+
+  def break_arr_component(lines)
+    lines.map! do |line|
+      line.split
+    end
+    lines
+  end
 
 end
 
